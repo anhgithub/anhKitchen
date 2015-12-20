522f09252d437e3e3dd111b343e048deb2ebc61a
anhgithub <akovacs1000@gmail.com>
2015-12-20 13:51:39 -0800

    Dynamic template

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 
 if __name__ == '__main__':
     app.debug = True
